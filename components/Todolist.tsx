e908868559dbfd12d16e79124cf64b5ef4415c48
Elena Mankova <liv_61@mail.ru>
2024-01-09 21:34:28 +0700

    added the ability to filter tasks by status

--- components/Todolist.tsx
+++ components/Todolist.tsx
@@ -1,4 +1,5 @@
-import React, {FC} from "react"
+import { type } from "@testing-library/user-event/dist/type"
+import React, { FC, useState } from "react"
 import { Button } from "./Button"
 
 export type TasksPropsType = {
@@ -12,33 +13,42 @@ type TodoListPropsType = {
 	tasks: Array<TasksPropsType>
 	onClickDeleteTask: (taskId: number) => void
 }
+type TaskStatusPropsType = "All" | "Active" | "Completed"
 
-export const TodoList: FC<TodoListPropsType> = ({title, tasks, onClickDeleteTask}) => {
-		const tasksList: Array<JSX.Element> = tasks.map( task => {
-			return(
-				<li key={task.id}>
-					<input type="checkbox" checked={task.isDone}  /> 
-					<span>{task.title}</span>
-					<Button title="Х" onclickHandler={()=> onClickDeleteTask(task.id)} />
-				</li>
-			)
-		})
+export const TodoList: FC<TodoListPropsType> = ({ title, tasks, onClickDeleteTask }) => {
+	const [statusTask, setStatusTask] = useState("All");
+
+	const changeShowTask = (status: TaskStatusPropsType) => {setStatusTask(status)};
+
+	const currentTasks = statusTask === "Active" ? tasks.filter(task => !task.isDone) :
+								statusTask === "Completed" ? tasks.filter(task => task.isDone) :
+								tasks;
+								
+	const tasksList: Array<JSX.Element> = currentTasks.map(task => {
 		return (
-			<div className="todo-list-class-name">
-                <h3>{title}</h3>
-                <div>
-                    <input/>
-                    <Button title="+" />
-                </div>
-                <ul>
-                   {tasksList}
-                </ul>
-                <div>
-                    {/* <Button title="All" onclickHandler={() => onclickHandler("All")}/> */}
-                    {/* <Button title="Active" onclickHandler={() => onclickHandler("Active")}/> */}
-                    {/* <Button title="Completed" onclickHandler={() => onclickHandler("Active")}/> */}
-                </div>
-
-            </div>
+			<li key={task.id}>
+				<input type="checkbox" checked={task.isDone} />
+				<span>{task.title}</span>
+				<Button title="Х" onclickHandler={() => onClickDeleteTask(task.id)} />
+			</li>
 		)
+	})
+	return (
+		<div className="todo-list-class-name">
+			<h3>{title}</h3>
+			<div>
+				<input />
+				<Button title="+" />
+			</div>
+			<ul>
+				{tasksList}
+			</ul>
+			<div>
+				<Button title="All" onclickHandler={() => changeShowTask("All")} />
+				<Button title="Active" onclickHandler={() => changeShowTask("Active")} />
+				<Button title="Completed" onclickHandler={() => changeShowTask("Completed")} />
+			</div>
+
+		</div>
+	)
 }
